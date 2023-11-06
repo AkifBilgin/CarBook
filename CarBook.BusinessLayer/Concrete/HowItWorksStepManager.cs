66540a29f7210b6392364852fe6e946daac9c886
Akif Bilgin <akif.bil@gmail.com>
2023-11-06 19:59:35 +0100

    Lesson 26

---
+++ CarBook.BusinessLayer/Concrete/HowItWorksStepManager.cs
@@ -0,0 +1,45 @@
+﻿using CarBook.BusinessLayer.Abstract;
+using CarBook.DataAccessLayer.Abstract;
+using CarBook.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.BusinessLayer.Concrete
+{
+    public class HowItWorksStepManager : IHowItWorksStepService
+    {
+        private readonly IHowItWorksStepDal _howItWorkStepDal;
+
+        public HowItWorksStepManager(IHowItWorksStepDal howItWorkStepDal)
+        {
+            _howItWorkStepDal = howItWorkStepDal;
+        }
+
+        public void TDelete(HowItWorksStep entity)
+        {
+            _howItWorkStepDal.Delete(entity);
+        }
+
+        public List<HowItWorksStep> TGetAll()
+        {
+            return _howItWorkStepDal.GetAll();
+        }
+
+        public HowItWorksStep TGetByID(int id)
+        {
+            return _howItWorkStepDal.GetByID(id);
+        }
+
+        public void TInsert(HowItWorksStep entity)
+        {
+            _howItWorkStepDal.Insert(entity);
+        }
+        public void TUpdate(HowItWorksStep entity)
+        {
+            _howItWorkStepDal.Update(entity);
+        }
+    }
+}
---
+++ CarBook.PresentationLayer/ViewComponents/ServiceComponents/_HowItWorksStepComponentPartial.cs
@@ -0,0 +1,22 @@
+﻿using CarBook.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace CarBook.PresentationLayer.ViewComponents.ServiceComponents
+{
+    public class _HowItWorksStepComponentPartial : ViewComponent
+    {
+        private readonly IHowItWorksStepService _howItWorksStepService;
+
+        public _HowItWorksStepComponentPartial(IHowItWorksStepService howItWorksStepService)
+        {
+            _howItWorksStepService = howItWorksStepService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _howItWorksStepService.TGetAll();
+            return View(values);
+        }
+    }
+}
